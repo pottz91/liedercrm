0c3b868aa2ac25cc9a896f15bf8f8390a179bab3
1djvik <130562431+1djvik@users.noreply.github.com>
2023-05-04 21:58:07 +0200

    Update lieder.php

--- lieder.php
+++ lieder.php
@@ -19,23 +19,28 @@ if (!isset($_SESSION["username"])) {
             <a href="seite2.php"><img src="img/home.svg"> Start</a>
             <a href="lieder.php"><img src="img/book.svg"> Lieder</a>
             <a href="liederadd.php"><img src="img/add.svg"> Lieder hinzufügen</a>
-            <a href="index.php?page=legal"><img src="img/legal.svg"> Impressum</a>
+            <a href="legal.php"><img src="img/legal.svg"> Impressum</a>
         </div>
         <div class="content">
             <h1>Lieder</h1>
             <p>Auf dieser Seite hast du einen Überblick über deine <b>Lieder</b></p>
+                <form method="post" action="suche.php">
+                    <label for="search">Suche:</label>
+                    <input type="text" id="search" name="search">
+                    <button type="submit">Suchen</button>
+                </form>
             <?php
         include 'datenbank.php';
 
-        $sql = "SELECT * FROM lieder ORDER by name ASC";
+        $sql = "SELECT * FROM lieder ORDER BY name ASC";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             echo "<table class='table'>";
-            echo "<thead><tr><th>Name</th><th>Autor</th></tr></thead>";
+            echo "<thead><tr><th>Name</th><th>Autor</th><th>Tonart</th></tr></thead>";
             echo "<tbody>";
             while($row = $result->fetch_assoc()) {
-                echo "<tr><td>" . $row["name"] . "</td><td>" . $row["autor"] . "</td></tr>";
+                echo "<tr><td>" . $row["name"] . "</td><td>" . $row["autor"] . "</td><td>" . $row["ton"] . "</td></tr>";
             }
             echo "</tbody>";
             echo "</table>";
