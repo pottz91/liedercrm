c995b52e14576aa3367a7e748daff1e379a548ff
C.Potthast <47056383+pottz91@users.noreply.github.com>
2023-06-02 13:45:25 +0200

    lieder loesch funktion

--- lieder.php
+++ lieder.php
@@ -6,13 +6,15 @@ include 'auth.php';
 
 <div id="wrapper">
     <?php
-    include 'menu.php'; ?>
+    include 'menu.php';
+    ?>
 
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
         <?php
-        include 'topbar.php'; ?>
+        include 'topbar.php';
+        ?>
         <!-- Main Content -->
         <div id="content">
 
@@ -21,19 +23,13 @@ include 'auth.php';
             <!-- Begin Page Content -->
             <div class="container-fluid">
 
-
-
-
                 <div class="main">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h2 mb-0 text-gray-800">Lieder</h1>
-
                     </div>
                     <div class="content">
                         <!-- Page Heading -->
-
                         <p>Auf dieser Seite hast du einen Überblick über deine <b>Lieder</b></p>
-
                         <form method="post" action="suche.php">
                             <label for="search">Suche:</label>
                             <input type="text" id="search" name="search">
@@ -42,22 +38,46 @@ include 'auth.php';
                         <?php
                         include 'datenbank.php';
 
+                        if (isset($_POST["lied_id"])) {
+                            $liedId = $_POST["lied_id"];
+
+                            // SQL-Statement zum Löschen des Lieds
+                            $sql = "DELETE FROM lieder WHERE id = $liedId";
+
+                            if ($conn->query($sql) === TRUE) {
+                                echo "<p>Lied wurde erfolgreich gelöscht.</p>";
+                            } else {
+                                echo "Fehler: " . $sql . "<br>" . $conn->error;
+                            }
+                        }
+
                         $sql = "SELECT * FROM lieder ORDER BY name ASC";
                         $result = $conn->query($sql);
 
                         if ($result->num_rows > 0) {
                             echo "<table class='table'>";
-                            echo "<thead><tr><th>Name</th><th>Autor</th><th>Tonart</th><th>Datei</th></tr></thead>";
+                            echo "<thead><tr><th>Name</th><th>Autor</th><th>Tonart</th><th>Datei</th><th>Aktionen</th></tr></thead>";
                             echo "<tbody>";
                             while ($row = $result->fetch_assoc()) {
-                                echo "<tr><td>" . $row["name"] . "</td><td>" . $row["autor"] . "</td><td>" . $row["ton"] . "</td><td>";
+                                echo "<tr>";
+                                echo "<td>" . $row["name"] . "</td>";
+                                echo "<td>" . $row["autor"] . "</td>";
+                                echo "<td>" . $row["ton"] . "</td>";
+                                echo "<td>";
                                 if (!empty($row["pdf_attachment"])) {
                                     $pdfPath = $row["pdf_attachment"];
                                     echo "<a href='$pdfPath' target='_blank' class='btn btn-primary'>PDF öffnen</a>";
                                 } else {
                                     echo "-";
                                 }
-                                echo "</td></tr>";
+                                echo "</td>";
+                                echo "<td>
+                                        <form method='post' action='lieder.php' onsubmit='return confirm(\"Möchtest du dieses Lied wirklich löschen?\");'>
+                                            <input type='hidden' name='lied_id' value='" . $row["id"] . "'>
+                                            <button type='submit' class='btn btn-danger'>Löschen</button>
+                                        </form>
+                                      </td>";
+                                echo "</tr>";
                             }
                             echo "</tbody>";
                             echo "</table>";
@@ -74,3 +94,5 @@ include 'auth.php';
         <?php
         include 'footer.php';
         ?>
+    </div>
+</div>
