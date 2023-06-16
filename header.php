05eba072fc73dd0406ccb687dbeccfcfc5474f0e
V. Hildebrandt <130562431+1djvik@users.noreply.github.com>
2023-06-16 09:53:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -20,3 +20,5 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=nunito:200,300,400,500,600,700,800,900|nunito-sans:400"
     rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
+    rel="stylesheet">
