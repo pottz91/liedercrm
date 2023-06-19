1993ceb3e49e4c409141e619f352910e231a71a6
V. Hildebrandt <130562431+1djvik@users.noreply.github.com>
2023-06-19 07:29:30 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/seite2.php">
@@ -27,47 +27,100 @@
         Interface
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link " href="lieder.php" aria-expanded="true">
             <i class="fas fa-fw fa-music"></i>
             <span class="menu-font">Lieder</span>
         </a>
-
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link " href="liederadd.php" aria-expanded="true">
             <i class="fas fa-fw fa-music"></i>
             <span class="menu-font">Lieder hinzufügen</span>
         </a>
-
     </li>
     <li class="nav-item">
         <a class="nav-link " href="logout.php" aria-expanded="true">
             <i class="fas fa-fw fa-sign-out"></i>
             <span class="menu-font">Logout</span>
         </a>
-
     </li>
-
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    <footer class="navbar navbar-dark bg-primary fixed-bottom d-md-none">
+    <a class="navbar-brand ml-auto order-2" href="#" onclick="toggleMobileMenu()">
+        <i class="fas fa-bars" style="display: inline-block;"></i>
+    </a>
+    <ul id="mobileMenuList" class="list-unstyled" style="display: none;">
+    
+    <li class="nav-item active">
+        <a class="nav-link" href="seite2.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span class="menu-font">Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
     </div>
 
 
+        <li class="nav-item">
+        <a class="nav-link " href="lieder.php" aria-expanded="true">
+            <i class="fas fa-fw fa-music"></i>
+            <span class="menu-font">Lieder</span>
+        </a>
+    </li>
+
+        <li class="nav-item">
+        <a class="nav-link " href="liederadd.php" aria-expanded="true">
+            <i class="fas fa-fw fa-music"></i>
+            <span class="menu-font">Lieder hinzufügen</span>
+        </a>
+    </li>
+
+        <li class="nav-item">
+        <a class="nav-link " href="logout.php" aria-expanded="true">
+            <i class="fas fa-fw fa-sign-out"></i>
+            <span class="menu-font">Logout</span>
+        </a>
+    </li>
+    </ul>
+</footer>
 
 </ul>
+
+<script>
+    function toggleMobileMenu() {
+        var mobileMenuList = document.getElementById('mobileMenuList');
+        
+        if (mobileMenuList.style.display === 'none') {
+            mobileMenuList.style.display = 'block';
+        } else {
+            mobileMenuList.style.display = 'none';
+        }
+    }
+</script>
 
 <style>
     .menu-font {
         font-size: 16px !important;
-        font-color: #fff !important;
+        color: #fff !important;
+    }
+
+    @media (max-width: 767px) {
+        .sidebar {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            z-index: 100;
+        }
     }
 </style>
