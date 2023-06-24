e245f98ecca83ab346c694c18e9744f49c48766b
V. Hildebrandt <130562431+1djvik@users.noreply.github.com>
2023-06-24 19:59:49 +0200

    Update menu.php
    Mobile Navigation im footer

--- menu.php
+++ menu.php
@@ -16,7 +16,8 @@
     <li class="nav-item active">
         <a class="nav-link" href="seite2.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span class="menu-font">Dashboard</span></a>
+            <span class="menu-font">Dashboard</span>
+        </a>
     </li>
 
     <!-- Divider -->
@@ -28,20 +29,21 @@
     </div>
 
     <li class="nav-item">
-        <a class="nav-link " href="lieder.php" aria-expanded="true">
+        <a class="nav-link" href="lieder.php" aria-expanded="true">
             <i class="fas fa-fw fa-music"></i>
             <span class="menu-font">Lieder</span>
         </a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link " href="liederadd.php" aria-expanded="true">
-            <i class="fas fa-fw fa-music"></i>
+        <a class="nav-link" href="liederadd.php" aria-expanded="true">
+            <i class="fas fa-fw fa-edit"></i>
             <span class="menu-font">Lieder hinzufügen</span>
         </a>
     </li>
+
     <li class="nav-item">
-        <a class="nav-link " href="logout.php" aria-expanded="true">
+        <a class="nav-link" href="logout.php" aria-expanded="true">
             <i class="fas fa-fw fa-sign-out"></i>
             <span class="menu-font">Logout</span>
         </a>
@@ -51,52 +53,35 @@
     <hr class="sidebar-divider d-none d-md-block">
 
     <footer class="navbar navbar-dark bg-primary fixed-bottom d-md-none">
-    <a class="navbar-brand ml-auto order-2" href="#" onclick="toggleMobileMenu()">
-        <i class="fas fa-bars" style="display: inline-block;"></i>
-    </a>
-    <div id="mobileMenuList">
-        <div class="row">
-            <div class="col">
-                <a class="nav-link" href="seite2.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span class="menu-font">Dashboard</span>
-                </a>
-            </div>
-            <div class="col">
-                <a class="nav-link" href="lieder.php">
-                    <i class="fas fa-fw fa-music"></i>
-                    <span class="menu-font">Lieder</span>
-                </a>
-            </div>
-            <div class="col">
-                <a class="nav-link" href="liederadd.php">
-                    <i class="fas fa-fw fa-music"></i>
-                    <span class="menu-font">Lieder hinzufügen</span>
-                </a>
-            </div>
-            <div class="col">
-                <a class="nav-link" href="logout.php">
-                    <i class="fas fa-fw fa-sign-out"></i>
-                    <span class="menu-font">Logout</span>
-                </a>
+        <div id="mobileMenuList" class="container">
+            <div class="row">
+            <div class="d-flex flex-row bd-highlight mb-3">...
+                <div class="p-2 bd-highlight"></div>
+                    <a class="nav-link" href="seite2.php">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span class="menu-font">Dashboard</span>
+                    </a>
+                <div class="p-2 bd-highlight"></div>
+                    <a class="nav-link" href="lieder.php">
+                        <i class="fas fa-fw fa-music"></i>
+                        <span class="menu-font">Lieder</span>
+                    </a>
+                <div class="p-2 bd-highlight"></div>
+                    <a class="nav-link" href="liederadd.php">
+                        <i class="fas fa-fw fa-edit"></i>
+                        <span class="menu-font">Bearbeiten</span>
+                    </a>
+                <div class="p-2 bd-highlight"></div>
+                    <a class="nav-link" href="logout.php">
+                        <i class="fas fa-fw fa-sign-out"></i>
+                        <span class="menu-font">Logout</span>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
 </ul>
-
-<script>
-    function toggleMobileMenu() {
-        var mobileMenuList = document.getElementById('mobileMenuList');
-        
-        if (mobileMenuList.style.display === 'none') {
-            mobileMenuList.style.display = 'block';
-        } else {
-            mobileMenuList.style.display = 'none';
-        }
-    }
-</script>
 
 <style>
     .menu-font {
@@ -105,12 +90,16 @@
     }
 
     @media (max-width: 767px) {
-        .sidebar {
+        ...sidebar {
             position: fixed;
             bottom: 0;
             left: 0;
             width: 100%;
             z-index: 100;
+            background-color: #0056b3;
+            background-image: linear-gradient(45deg, #0056b3, #00a9e0);
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
         }
     }
+
 </style>
