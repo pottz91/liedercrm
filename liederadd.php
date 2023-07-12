742cb9f33fde649b6bed24040a10dd8b5c50f1c4
C.Potthast <47056383+pottz91@users.noreply.github.com>
2023-07-12 10:22:08 +0200

    :bug: datums angabe fixed if else bedieung rein

--- liederadd.php
+++ liederadd.php
@@ -286,11 +286,12 @@ ON DUPLICATE KEY UPDATE gesamt_abspielungen = gesamt_abspielungen + 1";
                             <tbody>
                                 <?php
                                 // SQL-Statement zum Abrufen der Lieder aus der Datenbank mit deutschem Datums- und Uhrzeitformat
-                                $sql = "SELECT lieder.id, lieder.name, lieder.autor, lieder.ton, lieder.pdf_attachment, benutzer.benutzername, DATE_FORMAT(lieder_datum.datum, '%d.%m.%Y') AS hinzugefuegt_am_deutsch, COALESCE(abspielungen.gesamt_abspielungen, 0) AS gesamt_abspielungen
-            FROM lieder
-            LEFT JOIN benutzer ON lieder.benutzer_id = benutzer.id
-            LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id
-            LEFT JOIN (SELECT lieder_id, COUNT(*) AS gesamt_abspielungen FROM abspielungen GROUP BY lieder_id) AS abspielungen ON lieder.id = abspielungen.lieder_id";
+                                $sql = "SELECT lieder.id, lieder.name, lieder.autor, lieder.ton, lieder.pdf_attachment, benutzer.benutzername, COALESCE(DATE_FORMAT(lieder_datum.datum, '%d.%m.%Y'), DATE_FORMAT(lieder.hinzugefuegt_am, '%d.%m.%Y')) AS datum, COALESCE(abspielungen.gesamt_abspielungen, 0) AS gesamt_abspielungen
+                                FROM lieder
+                                LEFT JOIN benutzer ON lieder.benutzer_id = benutzer.id
+                                LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id
+                                LEFT JOIN (SELECT lieder_id, COUNT(*) AS gesamt_abspielungen FROM abspielungen GROUP BY lieder_id) AS abspielungen ON lieder.id = abspielungen.lieder_id";
+
 
                                 $result = $conn->query($sql);
 
@@ -315,10 +316,10 @@ ON DUPLICATE KEY UPDATE gesamt_abspielungen = gesamt_abspielungen + 1";
                         <button type='button' class='btn btn-sm btn-warning' data-bs-toggle='modal' data-bs-target='#editModal' data-id='" . $row["id"] . "' data-name='" . $row["name"] . "' data-autor='" . $row["autor"] . "' data-ton='" . $row["ton"] . "' data-pdf='" . $row["pdf_attachment"] . "'>Bearbeiten</button>
                         <form method='post' action='liederadd.php' onsubmit='return confirm(\"Möchtest du dieses Lied wirklich löschen?\")'>
                             <input type='hidden' name='lied_id' value='" . $row["id"] . "'>
-                            <button type='submit' style='font-size:12px' class='btn btn-sm btn-danger'>Löschen</button>
+                            <button type='submit' style='font-size:12px' class='btn btn-sm btn-danger'  >Löschen</button>
                         </form>
                     </td>
-                    <td>" . $row["hinzugefuegt_am_deutsch"] . "</td>
+                    <td>" . $row["datum"] . "</td>
                     <td>" . $row["gesamt_abspielungen"] . "</td>
                 </tr>";
                                     }
@@ -411,6 +412,11 @@ ON DUPLICATE KEY UPDATE gesamt_abspielungen = gesamt_abspielungen + 1";
         </script>
     </div>
 </div>
+
+
+
+
+
 </body>
 
 </html>
