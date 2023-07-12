806024af94ca0d94d1d8d79fe117b48d4b062c88
C.Potthast <47056383+pottz91@users.noreply.github.com>
2023-07-12 12:00:54 +0200

    :sparkles: neue statistik auswertung

---
+++ statistik.php
@@ -0,0 +1,117 @@
+<?php
+include 'datenbank.php';
+include 'auth.php';
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<body>
+    <div id="wrapper">
+        <?php include 'menu.php'; ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <?php include 'topbar.php'; ?>
+            <!-- Main Content -->
+            <div id="content" style="margin-bottom: 200px">
+                <form action="" method="get">
+                    <label for="zeitraum">Zeitraum:</label>
+                    <select name="zeitraum" id="zeitraum" onchange="this.form.submit()">
+                        <option value="alle" <?php echo isset($_GET['zeitraum']) && $_GET['zeitraum'] == 'alle' ? 'selected' : ''; ?>>Alle</option>
+                        <option value="14tage" <?php echo isset($_GET['zeitraum']) && $_GET['zeitraum'] == '14tage' ? 'selected' : ''; ?>>Letzte 14 Tage</option>
+                        <option value="30tage" <?php echo isset($_GET['zeitraum']) && $_GET['zeitraum'] == '30tage' ? 'selected' : ''; ?>>Letzte 30 Tage</option>
+                        <option value="60tage" <?php echo isset($_GET['zeitraum']) && $_GET['zeitraum'] == '60tage' ? 'selected' : ''; ?>>Letzte 60 Tage</option>
+                    </select>
+                </form>
+
+                <?php
+                // Filter für den Zeitraum
+                $zeitraum = isset($_GET['zeitraum']) ? $_GET['zeitraum'] : 'alle';
+
+                // SQL-Abfrage basierend auf dem gewählten Zeitraum
+                $sql = "";
+                if ($zeitraum == '14tage') {
+                    $sql = "SELECT lieder.name, COUNT(lieder_datum.id) AS abspielungen 
+                            FROM lieder 
+                            LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id 
+                            WHERE lieder_datum.datum >= DATE_SUB(CURDATE(), INTERVAL 14 DAY) 
+                            GROUP BY lieder.id 
+                            ORDER BY abspielungen DESC";
+                } elseif ($zeitraum == '30tage') {
+                    $sql = "SELECT lieder.name, COUNT(lieder_datum.id) AS abspielungen 
+                            FROM lieder 
+                            LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id 
+                            WHERE lieder_datum.datum >= DATE_SUB(CURDATE(), INTERVAL 30 DAY) 
+                            GROUP BY lieder.id 
+                            ORDER BY abspielungen DESC";
+                } elseif ($zeitraum == '60tage') {
+                    $sql = "SELECT lieder.name, COUNT(lieder_datum.id) AS abspielungen 
+                            FROM lieder 
+                            LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id 
+                            WHERE lieder_datum.datum >= DATE_SUB(CURDATE(), INTERVAL 60 DAY) 
+                            GROUP BY lieder.id 
+                            ORDER BY abspielungen DESC";
+                } else {
+                    $sql = "SELECT lieder.name, COUNT(lieder_datum.id) AS abspielungen 
+                            FROM lieder 
+                            LEFT JOIN lieder_datum ON lieder.id = lieder_datum.lied_id 
+                            GROUP BY lieder.id 
+                            ORDER BY abspielungen DESC";
+                }
+
+                // SQL-Abfrage ausführen
+                $result = $conn->query($sql);
+
+                // Überprüfen, ob die Abfrage erfolgreich war
+                if ($result === false) {
+                    die("Datenbankabfrage fehlgeschlagen: " . $conn->error);
+                }
+
+                // Chart-Daten erstellen
+                $chartData = [];
+                while ($row = $result->fetch_assoc()) {
+                    $chartData[] = [
+                        "x" => $row["name"],
+                        "y" => $row["abspielungen"]
+                    ];
+                }
+
+                // Datenbankverbindung schließen
+                $conn->close();
+                ?>
+
+                <!-- ApexCharts-Diagramm -->
+                <div id="chart"></div>
+
+                <script>
+                    // ApexCharts-Code
+                    document.addEventListener("DOMContentLoaded", function () {
+                        const options = {
+                            chart: {
+                                type: 'bar',
+                                height: 350,
+                                toolbar: {
+                                    show: false
+                                }
+                            },
+                            series: [{
+                                name: 'Abspielungen',
+                                data: <?php echo json_encode($chartData); ?>
+                            }],
+                            xaxis: {
+                                type: 'category',
+                            },
+                        };
+
+                        const chart = new ApexCharts(document.querySelector("#chart"), options);
+                        chart.render();
+                    });
+                </script>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
