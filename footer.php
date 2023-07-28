bc5e8e09795938efb218900d0304892dbf07ba91
V. Hildebrandt <130562431+1djvik@users.noreply.github.com>
2023-07-28 07:08:31 +0200

    Update Copyright.php

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <!-- Footer -->
-<footer class="sticky-footer bg-white">
+<footer class="footer-background sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
+            <span>Copyright &copy; ProJeCt 2023</span>
         </div>
     </div>
 </footer>
